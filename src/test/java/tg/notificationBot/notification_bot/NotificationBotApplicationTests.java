0a09dfe2cd0939fc7fcf61b8634db62201f481c8
Vitalii <lennab@mail.ru>
2023-08-03 16:56:26 +0300

    First Commit

---
+++ src/test/java/tg/notificationBot/notification_bot/NotificationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.notificationBot.notification_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
