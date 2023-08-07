4d4f214f9d7d14f3045efa51fb6520a19d63bdb9
Vitalii <lennab@mail.ru>
2023-08-07 13:22:18 +0300

    First Commit

---
+++ src/main/java/pro/sky/telegrambot/entities/NotificationTask.java
@@ -0,0 +1,75 @@
+package pro.sky.telegrambot.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "notification_task")
+public class NotificationTask {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long chatId;
+    private LocalDateTime dataTimeMessage;
+    private String message;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public LocalDateTime getDataTimeMessage() {
+        return dataTimeMessage;
+    }
+
+    public void setDataTimeMessage(LocalDateTime dataTimeMessage) {
+        this.dataTimeMessage = dataTimeMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationTask{" +
+                "id=" + id +
+                ", chatId=" + chatId +
+                ", dataTimeMessage=" + dataTimeMessage +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,11 +2,16 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.stereotype.Service;
+import com.pengrad.telegrambot.request.SendMessage;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -18,6 +23,8 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private TaskExecutionProperties taskExecutionProperties;
 
     @PostConstruct
     public void init() {
@@ -30,6 +37,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
             // Process your updates here
         });
+
+        for (var update : updates) {
+            if (update.message().text().equals("/start")) {
+
+
+                SendMessage message = new SendMessage(update.message().chat().id(), "Hi " + update.message().chat().firstName());
+
+                SendResponse response = telegramBot.execute(message);
+            }
+        }
+
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
